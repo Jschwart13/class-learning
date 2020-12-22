2a98227c7ca7cdd345bdde60eb132fbd4dacf2f4
Jschwart13 <49917183+Jschwart13@users.noreply.github.com>
2020-12-22 09:39:00 -0500

    Update HelloWorld.java
    added line from github

--- src/basics/HelloWorld.java
+++ src/basics/HelloWorld.java
@@ -3,7 +3,8 @@ package basics;
 public class HelloWorld {
 
     public static void main(String[] args) {
-	    System.out.println("Hello World");
+	System.out.println("Hello World");
         System.out.println("Adding code to my project");
+	System.out.println("Hi from Github");
     }
 }
